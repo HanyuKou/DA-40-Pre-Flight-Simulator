2791341c630beb03480cc4ae6212d5b8069c6a42
liyhy103 <liyhy103@mymail.unisa.edu.au>
2024-08-21 12:32:45 +0930

    Update UIController.cs
    update left-hand controller menu button script

--- Assets/Samples/XR Interaction Toolkit/2.4.3/Starter Assets/UIController.cs
+++ Assets/Samples/XR Interaction Toolkit/2.4.3/Starter Assets/UIController.cs
@@ -1,21 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-// Toggles the UI Canvas visibility using a button press
 public class UIController : MonoBehaviour
 {
     public InputActionReference menu;  // Action to toggle the menu
     public Canvas canvas;              // Canvas to show/hide
 
-    private void Start()
+    private void OnEnable()
     {
-        if(menu != null)
+        if (menu != null)
         {
             menu.action.Enable();  // Enable input action
             menu.action.performed += ToggleMenu;  // Call ToggleMenu when action is triggered
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (menu != null)
+        {
+            menu.action.performed -= ToggleMenu;  // Unsubscribe from event
+            menu.action.Disable();  // Disable input action
         }
     }
 
@@ -24,5 +29,3 @@ public class UIController : MonoBehaviour
         canvas.enabled = !canvas.enabled;  // Toggle Canvas visibility
     }
 }
-
-
